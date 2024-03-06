18e41f3674ea27a7d9cd026ec9095530ca5795df
Shannoff03 <viannyvas@gmail.com>
2024-03-06 12:56:04 +0530

    InitialCommit

---
+++ src/components/layout/MainLayout.jsx
@@ -0,0 +1,40 @@
+import {Box } from "@mui/material";
+import {Outlet} from "react-router-dom"
+import GlobalLoading from "../common/GlobalLoading";
+import Footer from "../common/Footer";
+import Topbar from "../common/TopBar";
+
+const MainLayout = () => {
+  return(
+    <>
+    {/* global loading */}
+    <GlobalLoading/>
+    {/* global loading */}
+
+    {/* login modal */}
+    {/* login modal */}
+
+    <Box display="flex" minHeight="100vh">
+      {/* header  */}
+      <Topbar />
+      {/* header  */}
+
+      {/* main  */}
+      <Box 
+      component="main"
+      flexGrow={1}
+      overflow="hidden"
+      minHeight="100vh">
+<Outlet/>
+      </Box>
+      {/* main  */}
+    </Box>
+
+    {/*  footer */}
+    <Footer/>
+    {/*  footer */}
+</>
+  );
+};
+
+export default MainLayout;
