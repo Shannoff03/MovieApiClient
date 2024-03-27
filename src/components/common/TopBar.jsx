b16ab6bb7c14b7c76d88259d954e35917844e7c9
Shannoff03 <viannyvas@gmail.com>
2024-03-27 09:11:07 +0530

    InitialCommit

--- src/components/common/TopBar.jsx
+++ src/components/common/TopBar.jsx
@@ -11,7 +11,7 @@ import { setAuthModalOpen } from "../../redux/features/authModalSlice";
 import { setThemeMode } from "../../redux/features/themeModeSlice";
 import Logo from "./Logo";
 import UserMenu from "./UserMenu";
-// import Sidebar from "./Sidebar";
+import Sidebar from "./Sidebar";
 
 const ScrollAppBar = ({ children, window }) => {
   const { themeMode } = useSelector((state) => state.themeMode);
@@ -47,7 +47,7 @@ const Topbar = () => {
 
   return (
     <>
-      {/* <Sidebar open={sidebarOpen} toggleSidebar={toggleSidebar} /> */}
+      <Sidebar open={sidebarOpen} toggleSidebar={toggleSidebar} />
       <ScrollAppBar>
         <AppBar elevation={0} sx={{ zIndex: 9999 }}>
           <Toolbar sx={{ alignItems: "center", justifyContent: "space-between" }}>
